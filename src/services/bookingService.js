15c78f80f65aa385888d8b93c78fe1190d9903f4
EL-Nabil-Hakimi <hakimi.deve@gmail.com>
2024-12-10 22:38:12 +0100

    Update booking service API URL to production endpoint

--- src/services/bookingService.js
+++ src/services/bookingService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://127.0.0.1:8000/api/bookings';
+const API_URL = 'https://barbershop.elhakimi.com/api/bookings';
 
 // Fetch all bookings
 export const fetchBookings = async () => {
